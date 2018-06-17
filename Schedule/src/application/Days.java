304a728e0533a12eaaf72e613ce6e440b0637224
tomerx21 <tomerx21@gmail.com>
2018-06-17 17:26:29 +0300

    days class

---
+++ Schedule/src/application/Days.java
@@ -0,0 +1,30 @@
+package application;
+
+public class Days {
+	private String name;
+	private int num;
+
+	public Days(int num) {
+		if (num == 1) {
+			this.num = num;
+		}
+		else if (num == 2) {
+			this.num = num;
+		}
+		else if (num == 3) {
+			this.num = num;
+		}
+		else if (num == 4) {
+			this.num = num;
+		}
+		else if (num == 5) {
+			this.num = num;
+		}
+		else if (num == 6) {
+			this.num = num;
+		}
+		else if (num == 7) {
+			
+		}
+	}
+}
