7bb46eff4af3eed43bfbdab899800edbc1e853d8
tomerx21 <tomerx21@gmail.com>
2018-06-17 19:39:58 +0300

    AS

--- Schedule/src/application/ScheduleController.java
+++ Schedule/src/application/ScheduleController.java
@@ -63,7 +63,7 @@ public class ScheduleController {
 
     @FXML
     public void initialize() {
-    	for (int i = 0; i < 8; i++) {
+    	for (int i = 1; i < 8; i++) {
 			days[i] = new Days(i);
 			dayCB.getItems().add(days[i]);
     	}
