01bf7418f6e4a6670f2614d34fd828b02295a26f
tomerx21 <tomerx21@gmail.com>
2018-06-17 17:26:50 +0300

    od day

--- Schedule/src/application/Days.java
+++ Schedule/src/application/Days.java
@@ -7,24 +7,31 @@ public class Days {
 	public Days(int num) {
 		if (num == 1) {
 			this.num = num;
+			this.name = "ראשון";
 		}
 		else if (num == 2) {
 			this.num = num;
+			this.name = "שני";
 		}
 		else if (num == 3) {
 			this.num = num;
+			this.name = "שלישי";
 		}
 		else if (num == 4) {
 			this.num = num;
+			this.name = "רביעי";
 		}
 		else if (num == 5) {
 			this.num = num;
+			this.name = "חמישי";
 		}
 		else if (num == 6) {
 			this.num = num;
+			this.name = "שישי";
 		}
 		else if (num == 7) {
-			
+			this.num = num;
+			this.name = "שבת";
 		}
 	}
 }
